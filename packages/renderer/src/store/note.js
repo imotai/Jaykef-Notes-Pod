de3c92bfa7578c564d1269e47732c4352a950710
imotai <codego.me@gmail.com>
2022-12-13 19:27:02 +0800

    fix: update

--- packages/renderer/src/store/note.js
+++ packages/renderer/src/store/note.js
@@ -1,21 +1,36 @@
-import { defineStore } from 'pinia';
-import { nanoid } from 'nanoid';
-import { useStorage } from '../composable/storage';
-import { DB3, getATestStaticKeypair, getAddress, DocStore, sign } from 'db3js';
+import { defineStore } from 'pinia'
+import { nanoid } from 'nanoid'
+import { useStorage } from '../composable/storage'
+import * as db3 from 'db3js'
+import dcrypto from "@deliberative/crypto"
+import { Buffer as BufferPolyfill } from 'buffer'
+globalThis.Buffer = BufferPolyfill;
+
+async function sign(data, privateKey) {
+    const signature = await dcrypto.sign(data, privateKey)
+    return signature
+}
+
+async function generateKey() {
+    const mnemonic = await dcrypto.generateMnemonic()
+    const keypair = await dcrypto.keyPairFromMnemonic(mnemonic)
+    return [keypair.secretKey, keypair.publicKey]
+}
 
 const storage = useStorage();
-const db3 = new DB3('http://127.0.0.1:26659');
-const doc_store = new DocStore(db3);
-const [sk, public_key] = (async () => {return await getATestStaticKeypair()})();
+const db3_sdk = new db3.DB3('http://127.0.0.1:26659');
+const doc_store = new db3.DocStore(db3_sdk);
+
 async function mySign(data) {
-	return [await sign(data, sk), public_key];
+ const [sk, public_key] = await generateKey();
+ return [await sign(data, sk), public_key];
 }
 
 const noteIndex = {
 	keys: [
 		{
 			name: 'id',
-			keyType: DocKeyType.STRING,
+			keyType: db3.DocKeyType.STRING,
 		},
 	],
 	ns: 'my_notes',
@@ -33,15 +48,23 @@ export const useNoteStore = defineStore('note', {
   actions: {
     retrieve() {
       return new Promise((resolve) => {
-        storage.get('notes', {}).then((data) => {
-          this.data = data;
-          resolve(data);
-        });
+          doc_store.queryDocsByRange(noteIndex, {
+                            id: '',
+                        },
+                          {
+                            id: '~',
+           }, mySign).then((docs)=> {
+                for (doc in docs) {
+                    this.data[doc[id]] = doc;
+                 }
+                resolve(docs);
+           });
       });
     },
     add(note = {}) {
       return new Promise((resolve) => {
         const id = note.id || nanoid();
+        console.log("add note id" + id);
         const newNote = {
           id,
           title: '',
@@ -53,37 +76,38 @@ export const useNoteStore = defineStore('note', {
           isArchived: false,
           ...note,
         };
-        this.data[id] = new_note;
-        doc_store.insertDocs(nodeIndex, [new_note], mySign);
+        this.data[id] = newNote;
+        const mid = doc_store.insertDocs(noteIndex, [newNote], mySign);
+        console.log(mid);
+        resolve(mid);
       });
     },
     update(id, data = {}) {
       return new Promise((resolve) => {
+
+        console.log("update note id" + id);
         const updatedNode =  {
           ...this.data[id],
           ...data,
         };
         this.data[id] = updatedNode;
-        doc_store.insertDocs(nodeIndex, [updatedNode], mySign);
+        const mid = doc_store.insertDocs(noteIndex, [updatedNode], mySign);
+        console.log(mid);
+        resolve(mid);
       });
     },
     async delete(id) {
       try {
         const lastEditedNote = localStorage.getItem('lastNoteEdit');
-
         if (lastEditedNote === id) localStorage.removeItem('lastNoteEdit');
-
         const { path, ipcRenderer } = window.electron;
         const dataDir = await storage.get('dataDir', '', 'settings');
-
         delete this.data[id];
-
         await ipcRenderer.callMain(
           'fs:remove',
           path.join(dataDir, 'notes-assets', id)
         );
         await storage.delete(`notes.${id}`);
-
         return id;
       } catch (error) {
         console.error(error);
